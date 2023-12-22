96aefbc5c31d5094921d9522c1b2823f837d00d1
BippyMiester <admin@jdswebservice.com>
2023-12-22 07:37:33 -0500

    Forgot a file for the last commit

--- app/Models/Auth/User.php
+++ app/Models/Auth/User.php
@@ -3,43 +3,27 @@
 namespace App\Models\Auth;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Server;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
+     * Create a new factory instance for the model.
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected static function newFactory(): Factory
+    {
+        return UserFactory::new();
+    }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function servers() {
+        return $this->hasMany(Server::class);
+    }
 }
