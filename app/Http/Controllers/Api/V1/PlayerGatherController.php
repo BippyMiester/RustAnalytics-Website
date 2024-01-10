2a6d5f90de7661e9de115a5b5aed1842f734dbb0
BippyMiester <admin@jdswebservice.com>
2024-01-10 11:40:38 -0500

    Added OnPlayerGather logic

--- app/Http/Controllers/Api/V1/PlayerGatherController.php
+++ app/Http/Controllers/Api/V1/PlayerGatherController.php
@@ -3,21 +3,36 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\PlayerGather;
 use App\Models\Server;
 use Illuminate\Http\Request;
 
 class PlayerGatherController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $playerGather = new PlayerGather;
+        $playerGather->server_id = $server->id;
+        $playerGather->username = $request->username;
+        $playerGather->steam_id = $request->steam_id;
+        $playerGather->resource = $request->resource;
+        $playerGather->amount = $request->amount;
+        $playerGather->save();
+
+        print($playerGather);
     }
 }
