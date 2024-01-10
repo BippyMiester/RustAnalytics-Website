7785b02beebe65cace450b28c53e26560377c506
BippyMiester <admin@jdswebservice.com>
2024-01-10 17:15:53 -0500

    Added PlayerDeaths Logic

--- app/Http/Controllers/Api/V1/PlayerDeathsController.php
+++ app/Http/Controllers/Api/V1/PlayerDeathsController.php
@@ -3,21 +3,39 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\AnimalKills;
+use App\Models\PlayerDeaths;
 use App\Models\Server;
 use Illuminate\Http\Request;
 
 class PlayerDeathsController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $playerDeath = new PlayerDeaths;
+        $playerDeath->server_id = $server->id;
+        $playerDeath->username = $request->username;
+        $playerDeath->steam_id = $request->steam_id;
+        $playerDeath->cause = $request->cause;
+        $playerDeath->x = $request->x;
+        $playerDeath->y = $request->y;
+        $playerDeath->grid = $request->grid;
+        $playerDeath->save();
+
+        print($playerDeath);
     }
 }
