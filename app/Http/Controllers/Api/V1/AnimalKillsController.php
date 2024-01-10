b2b587609357fb3ba366f2d8e4833eb0bd84c0b5
BippyMiester <admin@jdswebservice.com>
2024-01-10 16:42:00 -0500

    Added Animal Kill Logic

--- app/Http/Controllers/Api/V1/AnimalKillsController.php
+++ app/Http/Controllers/Api/V1/AnimalKillsController.php
@@ -3,21 +3,38 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\AnimalKills;
+use App\Models\DestroyedBuildings;
 use App\Models\Server;
 use Illuminate\Http\Request;
 
 class AnimalKillsController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $animalKill = new AnimalKills;
+        $animalKill->server_id = $server->id;
+        $animalKill->username = $request->username;
+        $animalKill->steam_id = $request->steam_id;
+        $animalKill->animal_type = $request->animal_type;
+        $animalKill->distance = $request->distance;
+        $animalKill->weapon = $request->weapon;
+        $animalKill->save();
+
+        print($animalKill);
     }
 }
