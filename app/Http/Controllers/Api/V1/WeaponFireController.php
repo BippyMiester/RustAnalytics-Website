5551fe862f53103707822c90376f600bb07da76a
BippyMiester <admin@jdswebservice.com>
2024-01-10 12:32:19 -0500

    Added WeaponFire logic

--- app/Http/Controllers/Api/V1/WeaponFireController.php
+++ app/Http/Controllers/Api/V1/WeaponFireController.php
@@ -4,20 +4,36 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Server;
+use App\Models\WeaponFire;
 use Illuminate\Http\Request;
 
 class WeaponFireController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $weaponFire = new WeaponFire;
+        $weaponFire->server_id = $server->id;
+        $weaponFire->username = $request->username;
+        $weaponFire->steam_id = $request->steam_id;
+        $weaponFire->bullet = $request->bullet;
+        $weaponFire->weapon = $request->weapon;
+        $weaponFire->amount = $request->amount;
+        $weaponFire->save();
+
+        print($weaponFire);
     }
 }
