9896cd0ce303ecf88bd25d922665d8baeb05621b
BippyMiester <admin@jdswebservice.com>
2024-01-10 14:44:20 -0500

    Added DestroyedContainers Logic

--- app/Http/Controllers/Api/V1/DestroyedContainersController.php
+++ app/Http/Controllers/Api/V1/DestroyedContainersController.php
@@ -3,21 +3,40 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\DestroyedContainers;
 use App\Models\Server;
 use Illuminate\Http\Request;
 
 class DestroyedContainersController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $destroyedContainer = new DestroyedContainers;
+        $destroyedContainer->server_id = $server->id;
+        $destroyedContainer->username = $request->username;
+        $destroyedContainer->steam_id = $request->steam_id;
+        $destroyedContainer->owner = $request->owner;
+        $destroyedContainer->type = $request->type;
+        $destroyedContainer->weapon = $request->weapon;
+        $destroyedContainer->grid = $request->grid;
+        $destroyedContainer->x = $request->x;
+        $destroyedContainer->y = $request->y;
+        $destroyedContainer->save();
+
+        print($destroyedContainer);
     }
 }
