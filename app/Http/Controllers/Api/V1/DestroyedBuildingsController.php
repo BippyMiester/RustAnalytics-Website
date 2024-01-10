1406e272c49024aeaeb7e5dc7e28b1a34ee02a49
BippyMiester <admin@jdswebservice.com>
2024-01-10 15:35:30 -0500

    Added DestroyedBuildings Logic

--- app/Http/Controllers/Api/V1/DestroyedBuildingsController.php
+++ app/Http/Controllers/Api/V1/DestroyedBuildingsController.php
@@ -3,21 +3,40 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\DestroyedBuildings;
+use App\Models\DestroyedContainers;
 use App\Models\Server;
 use Illuminate\Http\Request;
 
 class DestroyedBuildingsController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $destroyedBuilding = new DestroyedBuildings;
+        $destroyedBuilding->server_id = $server->id;
+        $destroyedBuilding->username = $request->username;
+        $destroyedBuilding->steam_id = $request->steam_id;
+        $destroyedBuilding->owner = $request->owner;
+        $destroyedBuilding->type = $request->type;
+        $destroyedBuilding->tier = $request->tier;
+        $destroyedBuilding->weapon = $request->weapon;
+        $destroyedBuilding->grid = $request->grid;
+        $destroyedBuilding->save();
+
+        print($destroyedBuilding);
     }
 }
