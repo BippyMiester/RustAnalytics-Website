92b6f58309000fb82b719ef55998e1a33d109a7c
BippyMiester <admin@jdswebservice.com>
2024-01-11 00:46:46 -0500

    Added PlayerCrafting Logic

--- app/Http/Controllers/Api/V1/PlayerCraftingController.php
+++ app/Http/Controllers/Api/V1/PlayerCraftingController.php
@@ -3,21 +3,36 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\PlayerCrafting;
 use App\Models\Server;
 use Illuminate\Http\Request;
 
 class PlayerCraftingController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $craftedItem = new PlayerCrafting;
+        $craftedItem->server_id = $server->id;
+        $craftedItem->username = $request->username;
+        $craftedItem->steam_id = $request->steam_id;
+        $craftedItem->item_crafted = $request->item_crafted;
+        $craftedItem->amount = $request->amount;
+        $craftedItem->save();
+
+        print($craftedItem);
     }
 }
