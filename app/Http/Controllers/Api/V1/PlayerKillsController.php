9a79bee07263a943f7320d8b25693a6fe5d4f863
BippyMiester <admin@jdswebservice.com>
2024-01-10 22:51:54 -0500

    Added PlayerKill Logic

--- app/Http/Controllers/Api/V1/PlayerKillsController.php
+++ app/Http/Controllers/Api/V1/PlayerKillsController.php
@@ -3,21 +3,40 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\PlayerKills;
 use App\Models\Server;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PlayerKillsController extends Controller
 {
-    public function checkServerAPIToken(Request $request) {
-        $server = Server::where('api_key', $request->api_key)->first();
-        if(!$server) {
-            $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
-            return false;
-        }
-        return true;
+    private function sendResponseCode(int $code, string $msg) {
+        return response()->json(['Response Message' => $msg], $code);
     }
 
-    private function sendResponseCode(int $code, string $msg) {
-        return response()->json(['message' => $msg], $code);
+    public function create(Request $request) {
+        // Check if request is null
+        if($request->all() == null) {
+            return $this->sendResponseCode(400, 'Data is null!');
+        }
+
+        // Grab the Server via the api Key
+        $server = Server::where('api_key', $request->api_key)->first();
+        if(!$server) {
+            return $this->sendResponseCode(400, 'Server API Key Invalid. Check your API key and try again.');
+        }
+
+        $playerKill = new PlayerKills;
+        $playerKill->server_id = $server->id;
+        $playerKill->username = $request->username;
+        $playerKill->steam_id = $request->steam_id;
+        $playerKill->kill_id = Str::uuid();
+        $playerKill->victim = $request->victim;
+        $playerKill->weapon = $request->weapon;
+        $playerKill->body_part = $request->body_part;
+        $playerKill->distance = $request->distance;
+        $playerKill->save();
+
+        print($playerKill);
     }
 }
