6ada838cf27677d22631deb3b2fa158d29aad8f8
BippyMiester <admin@jdswebservice.com>
2024-01-18 23:29:41 -0500

    Remove Unused Navbar Notifications and Links

--- resources/views/partials/dashboard/navbar.blade.php
+++ resources/views/partials/dashboard/navbar.blade.php
@@ -13,158 +13,158 @@
                     {{ Auth::user()->username }}
                 </a>
 
-                <ul class="dropdown-menu">
+{{--                <ul class="dropdown-menu">--}}
 
-                    <!-- Reverse Caret -->
-                    <li class="caret"></li>
+{{--                    <!-- Reverse Caret -->--}}
+{{--                    <li class="caret"></li>--}}
 
-                    <!-- Profile sub-links -->
-                    <li>
-                        <a href="extra-timeline.html">
-                            <i class="entypo-user"></i>
-                            Edit Profile
-                        </a>
-                    </li>
+{{--                    <!-- Profile sub-links -->--}}
+{{--                    <li>--}}
+{{--                        <a href="extra-timeline.html">--}}
+{{--                            <i class="entypo-user"></i>--}}
+{{--                            Edit Profile--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
 
-                    <li>
-                        <a href="mailbox.html">
-                            <i class="entypo-mail"></i>
-                            Inbox
-                        </a>
-                    </li>
+{{--                    <li>--}}
+{{--                        <a href="mailbox.html">--}}
+{{--                            <i class="entypo-mail"></i>--}}
+{{--                            Inbox--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
 
-                    <li>
-                        <a href="extra-calendar.html">
-                            <i class="entypo-calendar"></i>
-                            Calendar
-                        </a>
-                    </li>
+{{--                    <li>--}}
+{{--                        <a href="extra-calendar.html">--}}
+{{--                            <i class="entypo-calendar"></i>--}}
+{{--                            Calendar--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
 
-                    <li>
-                        <a href="#">
-                            <i class="entypo-clipboard"></i>
-                            Tasks
-                        </a>
-                    </li>
-                </ul>
+{{--                    <li>--}}
+{{--                        <a href="#">--}}
+{{--                            <i class="entypo-clipboard"></i>--}}
+{{--                            Tasks--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
             </li>
 
         </ul>
 
-        <ul class="user-info pull-left pull-right-xs pull-none-xsm">
+{{--        <ul class="user-info pull-left pull-right-xs pull-none-xsm">--}}
 
-            <!-- Raw Notifications -->
-            <li class="notifications dropdown">
+{{--            <!-- Raw Notifications -->--}}
+{{--            <li class="notifications dropdown">--}}
 
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                    <i class="entypo-attention"></i>
-                    <span class="badge badge-info">6</span>
-                </a>
+{{--                <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">--}}
+{{--                    <i class="entypo-attention"></i>--}}
+{{--                    <span class="badge badge-info">6</span>--}}
+{{--                </a>--}}
 
-                <ul class="dropdown-menu">
-                    <li class="top">
-                        <p class="small">
-                            <a href="#" class="pull-right">Mark all Read</a>
-                            You have <strong>3</strong> new notifications.
-                        </p>
-                    </li>
+{{--                <ul class="dropdown-menu">--}}
+{{--                    <li class="top">--}}
+{{--                        <p class="small">--}}
+{{--                            <a href="#" class="pull-right">Mark all Read</a>--}}
+{{--                            You have <strong>3</strong> new notifications.--}}
+{{--                        </p>--}}
+{{--                    </li>--}}
 
-                    <li>
-                        <ul class="dropdown-menu-list scroller">
-                            <li class="unread notification-success">
-                                <a href="#">
-                                    <i class="entypo-user-add pull-right"></i>
+{{--                    <li>--}}
+{{--                        <ul class="dropdown-menu-list scroller">--}}
+{{--                            <li class="unread notification-success">--}}
+{{--                                <a href="#">--}}
+{{--                                    <i class="entypo-user-add pull-right"></i>--}}
 
-                                    <span class="line">
-												<strong>New user registered</strong>
-											</span>
+{{--                                    <span class="line">--}}
+{{--												<strong>New user registered</strong>--}}
+{{--											</span>--}}
 
-                                    <span class="line small">
-												30 seconds ago
-											</span>
-                                </a>
-                            </li>
+{{--                                    <span class="line small">--}}
+{{--												30 seconds ago--}}
+{{--											</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
 
-                            <li class="unread notification-secondary">
-                                <a href="#">
-                                    <i class="entypo-heart pull-right"></i>
+{{--                            <li class="unread notification-secondary">--}}
+{{--                                <a href="#">--}}
+{{--                                    <i class="entypo-heart pull-right"></i>--}}
 
-                                    <span class="line">
-												<strong>Someone special liked this</strong>
-											</span>
+{{--                                    <span class="line">--}}
+{{--												<strong>Someone special liked this</strong>--}}
+{{--											</span>--}}
 
-                                    <span class="line small">
-												2 minutes ago
-											</span>
-                                </a>
-                            </li>
+{{--                                    <span class="line small">--}}
+{{--												2 minutes ago--}}
+{{--											</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
 
-                            <li class="notification-primary">
-                                <a href="#">
-                                    <i class="entypo-user pull-right"></i>
+{{--                            <li class="notification-primary">--}}
+{{--                                <a href="#">--}}
+{{--                                    <i class="entypo-user pull-right"></i>--}}
 
-                                    <span class="line">
-												<strong>Privacy settings have been changed</strong>
-											</span>
+{{--                                    <span class="line">--}}
+{{--												<strong>Privacy settings have been changed</strong>--}}
+{{--											</span>--}}
 
-                                    <span class="line small">
-												3 hours ago
-											</span>
-                                </a>
-                            </li>
+{{--                                    <span class="line small">--}}
+{{--												3 hours ago--}}
+{{--											</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
 
-                            <li class="notification-danger">
-                                <a href="#">
-                                    <i class="entypo-cancel-circled pull-right"></i>
+{{--                            <li class="notification-danger">--}}
+{{--                                <a href="#">--}}
+{{--                                    <i class="entypo-cancel-circled pull-right"></i>--}}
 
-                                    <span class="line">
-												John cancelled the event
-											</span>
+{{--                                    <span class="line">--}}
+{{--												John cancelled the event--}}
+{{--											</span>--}}
 
-                                    <span class="line small">
-												9 hours ago
-											</span>
-                                </a>
-                            </li>
+{{--                                    <span class="line small">--}}
+{{--												9 hours ago--}}
+{{--											</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
 
-                            <li class="notification-info">
-                                <a href="#">
-                                    <i class="entypo-info pull-right"></i>
+{{--                            <li class="notification-info">--}}
+{{--                                <a href="#">--}}
+{{--                                    <i class="entypo-info pull-right"></i>--}}
 
-                                    <span class="line">
-												The server is status is stable
-											</span>
+{{--                                    <span class="line">--}}
+{{--												The server is status is stable--}}
+{{--											</span>--}}
 
-                                    <span class="line small">
-												yesterday at 10:30am
-											</span>
-                                </a>
-                            </li>
+{{--                                    <span class="line small">--}}
+{{--												yesterday at 10:30am--}}
+{{--											</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
 
-                            <li class="notification-warning">
-                                <a href="#">
-                                    <i class="entypo-rss pull-right"></i>
+{{--                            <li class="notification-warning">--}}
+{{--                                <a href="#">--}}
+{{--                                    <i class="entypo-rss pull-right"></i>--}}
 
-                                    <span class="line">
-												New comments waiting approval
-											</span>
+{{--                                    <span class="line">--}}
+{{--												New comments waiting approval--}}
+{{--											</span>--}}
 
-                                    <span class="line small">
-												last week
-											</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+{{--                                    <span class="line small">--}}
+{{--												last week--}}
+{{--											</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </li>--}}
 
-                    <li class="external">
-                        <a href="#">View all notifications</a>
-                    </li>
-                </ul>
+{{--                    <li class="external">--}}
+{{--                        <a href="#">View all notifications</a>--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
 
-            </li>
+{{--            </li>--}}
 
-        </ul>
+{{--        </ul>--}}
 
     </div>
 
