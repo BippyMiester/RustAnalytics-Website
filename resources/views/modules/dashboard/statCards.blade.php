4ae7ab6b7aa56686f9ba2dafba7a4ce6cfb99091
BippyMiester <admin@jdswebservice.com>
2024-01-19 00:19:57 -0500

    Removed Stat Cards From Dashboard

--- resources/views/modules/dashboard/statCards.blade.php
+++ resources/views/modules/dashboard/statCards.blade.php
@@ -11,41 +11,41 @@
 
     </div>
 
-    <div class="col-sm-3 col-xs-6">
+{{--    <div class="col-sm-3 col-xs-6">--}}
 
-        <div class="tile-stats tile-green">
-            <div class="icon"><i class="entypo-chart-bar"></i></div>
-            <div class="num" data-start="0" data-end="135" data-postfix="" data-duration="1500" data-delay="600">0</div>
+{{--        <div class="tile-stats tile-green">--}}
+{{--            <div class="icon"><i class="entypo-chart-bar"></i></div>--}}
+{{--            <div class="num" data-start="0" data-end="135" data-postfix="" data-duration="1500" data-delay="600">0</div>--}}
 
-            <h3>Players</h3>
-            <p>have connected to your server</p>
-        </div>
+{{--            <h3>Players</h3>--}}
+{{--            <p>have connected to your server</p>--}}
+{{--        </div>--}}
 
-    </div>
+{{--    </div>--}}
 
-    <div class="clear visible-xs"></div>
+{{--    <div class="clear visible-xs"></div>--}}
 
-    <div class="col-sm-3 col-xs-6">
+{{--    <div class="col-sm-3 col-xs-6">--}}
 
-        <div class="tile-stats tile-aqua">
-            <div class="icon"><i class="entypo-mail"></i></div>
-            <div class="num" data-start="0" data-end="23" data-postfix="" data-duration="1500" data-delay="1200">0</div>
+{{--        <div class="tile-stats tile-aqua">--}}
+{{--            <div class="icon"><i class="entypo-mail"></i></div>--}}
+{{--            <div class="num" data-start="0" data-end="23" data-postfix="" data-duration="1500" data-delay="1200">0</div>--}}
 
-            <h3>Animals</h3>
-            <p>have been killed</p>
-        </div>
+{{--            <h3>Animals</h3>--}}
+{{--            <p>have been killed</p>--}}
+{{--        </div>--}}
 
-    </div>
+{{--    </div>--}}
 
-    <div class="col-sm-3 col-xs-6">
+{{--    <div class="col-sm-3 col-xs-6">--}}
 
-        <div class="tile-stats tile-blue">
-            <div class="icon"><i class="entypo-rss"></i></div>
-            <div class="num" data-start="0" data-end="52" data-postfix="" data-duration="1500" data-delay="1800">0</div>
+{{--        <div class="tile-stats tile-blue">--}}
+{{--            <div class="icon"><i class="entypo-rss"></i></div>--}}
+{{--            <div class="num" data-start="0" data-end="52" data-postfix="" data-duration="1500" data-delay="1800">0</div>--}}
 
-            <h3>Subscribers</h3>
-            <p>on our site right now.</p>
-        </div>
+{{--            <h3>Subscribers</h3>--}}
+{{--            <p>on our site right now.</p>--}}
+{{--        </div>--}}
 
-    </div>
+{{--    </div>--}}
 </div>
