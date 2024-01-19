41f04113ae497ccd1094501519803c7bc7efbf5c
BippyMiester <admin@jdswebservice.com>
2024-01-19 04:24:52 -0500

    Removed Unused Classes

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\PlayerGather;
-use App\Models\PlayerKills;
 use App\Models\Server;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
