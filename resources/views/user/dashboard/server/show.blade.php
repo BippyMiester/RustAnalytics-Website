0e1dfab12131f2d7b0cab4164242b0f336e1f187
BippyMiester <admin@jdswebservice.com>
2024-02-10 00:12:23 -0500

    Marked Gathering, Kills, and Weapon Fire as "Coming Soon"

--- resources/views/user/dashboard/server/show.blade.php
+++ resources/views/user/dashboard/server/show.blade.php
@@ -206,7 +206,7 @@
                 </div>
                 <div class="card-body text-center">
                     <h5 class="card-title">Player Gathering</h5>
-                    <a href="#" class="btn btn-primary">View</a>
+                    <a href="#" class="btn btn-secondary">Coming Soon</a>
                 </div>
                 <div class="card-arrow">
                     <div class="card-arrow-top-left"></div>
@@ -223,7 +223,7 @@
                 </div>
                 <div class="card-body text-center">
                     <h5 class="card-title">Player Kills</h5>
-                    <a href="#" class="btn btn-primary">View</a>
+                    <a href="#" class="btn btn-secondary">Coming Soon</a>
                 </div>
                 <div class="card-arrow">
                     <div class="card-arrow-top-left"></div>
@@ -240,7 +240,7 @@
                 </div>
                 <div class="card-body text-center">
                     <h5 class="card-title">Weapon Fire</h5>
-                    <a href="#" class="btn btn-primary">View</a>
+                    <a href="#" class="btn btn-secondary">Coming Soon</a>
                 </div>
                 <div class="card-arrow">
                     <div class="card-arrow-top-left"></div>
