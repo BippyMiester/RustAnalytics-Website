f1842fff5898d7bef491b627d13747c458e771e7
BippyMiester <admin@jdswebservice.com>
2024-02-15 02:31:24 -0500

    Test Admin Stuff

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -16,6 +16,9 @@ class AdminAuthenticate
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if($request->is('admin/logviewer')) {
+            dd(Auth::user());
+        }
         if(Auth::user()->admin) {
             return $next($request);
         }
