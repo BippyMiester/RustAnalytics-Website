ccfc4d3d6d03c5850e879c3b348871c940cfd3a7
BippyMiester <admin@jdswebservice.com>
2024-02-15 04:01:12 -0500

    Updated Pagination On Player Bans Page

--- app/Http/Controllers/User/DashboardBansController.php
+++ app/Http/Controllers/User/DashboardBansController.php
@@ -23,7 +23,7 @@ class DashboardBansController extends Controller
             $allBans = $allBans->merge($serverBans); // Merge current server bans into the allBans collection
         }
 
-        $page = $request->input('page', 1);
+        $page = $request->input('page', 25);
         $perPage = 1;
         $offset = ($page - 1) * $perPage;
         $currentPageBans = $allBans->slice($offset, $perPage)->all();
