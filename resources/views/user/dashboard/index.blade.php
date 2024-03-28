1eae3bd8d64d6345c86fb4a5dec4aeb035dfd825
BippyMiester <admin@jdswebservice.com>
2024-03-28 13:58:20 -0400

    Added Russia Warning to Dashboard

--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -5,6 +5,13 @@
 @section('content')
 
     <div class="row justify-content-center">
+        <div class="col-sm-12 text-center">
+            <div class="alert alert-warning">
+                Warning: If you server is located in Russia, all servers in Russia are now blocked due to massive amounts of API abuse. Almost 17 million requests were placed within a 24 hour period. For perspective, the next closest highest amount of requests was the USA with 34,000. So Russia is now banned. Please place your server in a country that doesn't promote API abuse, or tries to actively DDOS our service.
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
         <div class="col-sm-6 text-center">
             <h1>Coming Soon!</h1>
             <p>Currently this page is under construction. Please click on the server page on the left for more information on your server.</p>
