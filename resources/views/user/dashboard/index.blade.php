b9e6070867b55509bba360ad47039ad1909ad2f2
BippyMiester <admin@jdswebservice.com>
2024-03-28 13:59:40 -0400

    Fixed Typos

--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-sm-12 text-center">
             <div class="alert alert-warning">
-                Warning: If you server is located in Russia, all servers in Russia are now blocked due to massive amounts of API abuse. Almost 17 million requests were placed within a 24 hour period. For perspective, the next closest highest amount of requests was the USA with 34,000. So Russia is now banned. Please place your server in a country that doesn't promote API abuse, or tries to actively DDOS our service.
+                Warning: If your server is located in Russia, all servers in Russia are now blocked due to massive amounts of API abuse. Almost 17 million requests were placed within a 24 hour period, for numerous weeks on end. For perspective, the next closest highest amount of requests was the USA with 34,000. So Russia is now banned. Please place your server in a country that doesn't promote API abuse, or tries to actively DDOS our service.
             </div>
         </div>
     </div>
